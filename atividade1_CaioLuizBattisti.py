bb267bd8b9e6473ea86121e7e346ccf45da4b870
CaioBattisti <cluizbattisti@gmail.com>
2025-02-21 20:03:11 -0300

    fazendo a 1

--- atividade1_CaioLuizBattisti.py
+++ atividade1_CaioLuizBattisti.py
@@ -2,6 +2,4 @@
 #Peça ao usuário para inserir um dos países que foram mostrados a ele e, em seguida
 #exibir o número de índice (ou seja, posição na lista) desse item na tupla.
 paises = ("Brasil","russia","canada","china","mexico")
-("0","1","2","3","4","5") = paises
-for i, pais in enumerate(paises):
-    print(paises)
+pais =input("Digite um pais: ").get(('livros'))
