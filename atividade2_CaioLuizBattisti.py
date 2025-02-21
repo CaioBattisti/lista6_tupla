b9320ccb89046352051fae67bcf420dbbe79efde
CaioBattisti <cluizbattisti@gmail.com>
2025-02-21 20:44:55 -0300

    fazendo a 2

--- atividade2_CaioLuizBattisti.py
+++ atividade2_CaioLuizBattisti.py
@@ -1,1 +1,9 @@
-#
+#Faça um programa que o usuario insira 10 produtos numa tupla. Exiba todos os produtos
+#solicite ao usuário para digitar um nome de produto e exiba a posição dele
+#em seguida peça para digitar numero entre 0 e 9 e exiba o produto da tupla.
+for i in range(10):
+    produtos = print("Digite o nome do produto {i+1}:")
+    print("\nLista de Produtos:",produtos)
+    produto_usuario =input("Digite um produto exibido")
+if produto_usuario in produtos:
+    print(f"O produto {produto_usuario} esta na posição {produtos.index(produto_usuario)}")
