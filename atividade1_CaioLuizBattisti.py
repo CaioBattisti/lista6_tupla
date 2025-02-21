6244f43b5d0186ed15a01921ebed29e76f3ade1f
CaioBattisti <cluizbattisti@gmail.com>
2025-02-21 19:12:53 -0300

    atualização

--- atividade1_CaioLuizBattisti.py
+++ atividade1_CaioLuizBattisti.py
@@ -2,5 +2,6 @@
 #Peça ao usuário para inserir um dos países que foram mostrados a ele e, em seguida
 #exibir o número de índice (ou seja, posição na lista) desse item na tupla.
 paises = ("Brasil","russia","canada","china","mexico")
+("0","1","2","3","4","5") = paises
 for i, pais in enumerate(paises):
     print(paises)
