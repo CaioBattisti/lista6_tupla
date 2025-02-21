0dd501f59b2843c0979a2d03f176fb04cee7003a
CaioBattisti <cluizbattisti@gmail.com>
2025-02-21 18:54:54 -0300

    fazendo

---
+++ atividade1_CaioLuizBattisti.py
@@ -0,0 +1,6 @@
+#Crie uma tupla contendo os nomes de cinco países e exiba toda a tupla. 
+#Peça ao usuário para inserir um dos países que foram mostrados a ele e, em seguida
+#exibir o número de índice (ou seja, posição na lista) desse item na tupla.
+paises = ("Brasil","russia","canada","china","mexico")
+for i, pais in enumerate(paises):
+    print(paises)
