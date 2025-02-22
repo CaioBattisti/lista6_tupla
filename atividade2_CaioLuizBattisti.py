e8c077d083e9905078e0edf09e1303c6ec8e69ba
CaioBattisti <cluizbattisti@gmail.com>
2025-02-21 21:51:42 -0300

    finalizado a 2

--- atividade2_CaioLuizBattisti.py
+++ atividade2_CaioLuizBattisti.py
@@ -1,18 +1,18 @@
 #Faça um programa que o usuario insira 10 produtos numa tupla. Exiba todos os produtos
 #solicite ao usuário para digitar um nome de produto e exiba a posição dele
 #em seguida peça para digitar numero entre 0 e 9 e exiba o produto da tupla.
-produtos =()
-for i in range(10):
-    produtos = input("Digite o nome do produto: ")
-    print("\nLista de Produtos:",produtos)
-produto_usuario =input("Digite um produto exibido: ")
-if produto_usuario in produtos:
-    print(f"O produto {produto_usuario} esta na posição {produtos.index(produto_usuario)}")
-else:
-    print("este produto nao esta na lista!")
-indice = -1
-while indice not in range(10):
-    indice =int(input("digite um numero entre 0 e 9: "))
-    print(f"O produto na posição {indice} é: {produtos[indice]}")
-if indice not in range(10):
-    print("Erro,Tente novamente!")
+produtos =tuple(input(f"Digite o nome do produto {i+1}: ")for i in range(10))
+print("\nprodutos inseridos: ")
+for produto in produtos:
+    print(produto)
+    nome_produto = input("\nDigite o nome de um produto: ")
+    if nome_produto in produto:
+        print(f"O produto '{nome_produto}' esta na posição: {produtos.index(nome_produto)}")
+    else:
+        print(f"O produto '{nome_produto}' nao foi encontrado!")
+    try:
+        indice = int(input("\nDigite o produto entre 0 e 9: "))
+        if indice <= 0 and indice < 10:
+            print(f"O produto na posição {indice} é '{produto[indice]}'")
+    except ValueError:
+            print("Entrada invalida,tente de novo!")
