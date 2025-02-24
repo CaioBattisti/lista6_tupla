609e4aa4e462ff1020eba5e86a3fe603c6b0a374
CaioBattisti <cluizbattisti@gmail.com>
2025-02-24 18:49:07 -0300

    finalizado a 3

--- atividade3CaioLuizBattisti.py
+++ atividade3CaioLuizBattisti.py
@@ -16,14 +16,19 @@ while True:
 nomes += tuple(novos_nomes)
 print(f"voce convidou {len(nomes)} pessoas para a festa.")
 print("Lista de convidados:",nomes)
+print("Caioluizbattisti")
 nome_procurado = input("Digite um nome da lista para procura-lo: ")
 if nome_procurado in nomes:
     print(f"O nome {nome_procurado} esta na posição {nomes.index(nome_procurado)} da lista.")
+    print("Caioluizbattisti")
 else:
     print("O nome digitado não esta na lista!")
+    print("Caioluizbattisti")
 remover = input(f"Deseja remover {nome_procurado} da lista? (s/n): ").strip().lower()
 if remover == "s" and nome_procurado in nomes:
     nomes = tuple(nome for nome in nomes if nome != nome_procurado)
     print("lista atualizada ",nomes)
+    print("Caioluizbattisti")
 else:
     print("nenhuma alteração feita na lista.")
+    print("Caioluizbattisti")
